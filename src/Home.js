6920c141a22c906e756df44579a86625a652860a
aberiggs <sprocket710@gmail.com>
2021-09-25 01:06:02 -0400

    First push of website

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h>
+          Home Page
+        </h>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
