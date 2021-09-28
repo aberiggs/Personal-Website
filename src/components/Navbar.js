ab672b48b6bbb21fdbd3340f6a6a48ba07d507d2
aberiggs <sprocket710@gmail.com>
2021-09-28 00:03:38 -0400

    (Slightly) better navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,17 @@ import "./Navbar.css";
 function Navbar() {
     return (
         <div className="Navbar">
-            <header className="Navbar-header">
-                <nav className="Navbar-nav">
-                    <ul>
-                        <li className="Navbar-home"><Link to="/" className="Navbar-home-link">Abe Riggs</Link></li>
-                        <li><Link to="/projects">Projects</Link></li> 
-                    </ul>
-                </nav>
-            </header>
+            <div className="Navbar-home">
+                <ul>
+                    <li><Link to="/" className="Navbar-home-link">Abe Riggs</Link></li>
+                </ul>
+            </div>
+            <div className="Navbar-main">
+                <ul>
+                    <li><Link to="/projects">Projects</Link></li> 
+                    <li><Link to="/projects">Test</Link></li> 
+                </ul>
+            </div>     
         </div>
     );
 
