9a07402b1c55501cf6eb2c0af3ddb15d88210988
aberiggs <34726295+aberiggs@users.noreply.github.com>
2021-09-27 22:50:07 -0400

    Some navbar work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import {Link} from "react-router-dom";
 
+import "./Navbar.css";
+
 function Navbar() {
     return (
         <div className="Navbar">
             <header className="Navbar-header">
                 <nav className="Navbar-nav">
-                    <li>
-                        <Link to="/" className="Navbar-home-link">Abe Riggs</Link>
-                    </li>
-                    <li>
-                        <Link to="/projects">Projects</Link>
-                    </li>    
+                    <ul>
+                        <li className="Navbar-home"><Link to="/" className="Navbar-home-link">Abe Riggs</Link></li>
+                        <li><Link to="/projects">Projects</Link></li> 
+                    </ul>
                 </nav>
             </header>
         </div>
