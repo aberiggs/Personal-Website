b0efda398c1633f57088d67adcbe160f6a9872c2
aberiggs <sprocket710@gmail.com>
2021-10-24 03:03:45 -0400

    Rebuilt home page and removed navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   Route
 } from "react-router-dom";
 
-import Navbar from "./components/Navbar.js";
 import Home from "./components/Home.js";
 import Projects from "./components/Projects.js";
 import Contact from "./components/Contact.js"
@@ -13,9 +12,6 @@ import Contact from "./components/Contact.js"
 export default function App() {
   return (
     <Router>
-
-      <Navbar />
-
       <Switch>
 
         <Route exact path="/">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,40 @@
+import styled from 'styled-components';
+
 import "./Home.css"
+
 import Portrait from "./SprocketPic.jpg"
+import { RiGithubFill } from 'react-icons/ri';
+import { IoLogoLinkedin } from 'react-icons/io';
+
+const Button = styled.a` 
+  display: flex;
+  border-radius: 15px;
+  padding: 0.25rem .5rem;
+  margin: 0.5rem 1rem;
+  width: auto;
+  background: transparent;
+  font-size: 2vmin;
+  color: #FAF6E9;
+  background-color: #494949;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+`;
 
 function Home() {
   return (
     <div className="Home">
-      <div className ="Home-top"> 
-        <div className="Home-top-left"></div>
-        <div className="Home-top-content">
-          <h1 className="Home-name">Abe Riggs IV</h1>
-          <img className="Home-portrait" src={Portrait} alt="Portrait"></img>
-          <h2>About Me</h2>
-        </div>
-        <div className="Home-top-right"></div>
+      <img className="Home-portrait" src={Portrait} alt="Portrait"></img>
+      <h1 className="Home-name">
+        Abe Riggs IV
+      </h1>
+      <h2 className="Home-about">
+        Student, Software Developer, Keyboard Enthusiast, Musician
+      </h2>
+      <div className="Home-buttons">
+        <Button href="https://github.com/aberiggs" target="_blank"><RiGithubFill className="Home-logos"/>Github</Button>
+        <Button href="https://www.linkedin.com/in/abe-riggs-iv-826493187/" target="_blank"><IoLogoLinkedin className="Home-logos"/>LinkedIn</Button>
       </div>
-      <div className="Home-bottom">
-
-      </div>
-     
     </div>
   );
 }
